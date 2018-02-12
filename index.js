23dcb230236ea43a84f8651ccdadab0b87da70cd
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-11 19:02:57 -0500

    adds database connection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGOURI);
 
 app.use('/assets', express.static(__dirname + '/public'));
 
